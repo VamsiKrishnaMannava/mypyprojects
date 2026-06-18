8cea1f7f88799d8297b267c959747b61acadf25b
vamsimannava <vamsimannava369@gmail.com>
2026-06-18 07:14:10 +0100

    ave-1

--- apps/datacleaning.py
+++ apps/datacleaning.py
@@ -1,2 +1,16 @@
-  git config --global user.email "vamsimannava369@gmail.com"
-  git config --global user.name "vamsimannava"
+import pandas as pd
+
+from ydata_profiling import ProfileReport
+
+#Load data
+df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv")
+
+print(df.head())
+
+
+#EDA
+profile = ProfileReport(df, title="Data Profiling Report", explorative=True)
+profile.to_file("data_profile.html")
+
+
+
