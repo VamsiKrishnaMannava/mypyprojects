b7fac6fa8075ac11a0ca7eb65cdc37b2f823899d
vamsimannava <vamsimannava369@gmail.com>
2026-06-23 05:50:17 +0100

    changes added to the data profiling module and a new ML program added to the apps/ml directory.

---
+++ apps/ml/ml_prog_1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+# pyrefly: ignore [missing-import]
+import joblib
+import os
+import time
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import
+
+
